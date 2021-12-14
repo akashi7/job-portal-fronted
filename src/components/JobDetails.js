5446b7ed646fa9391231b04f74e14b6cc0f8e574
akashi7 <christiannseko@gmail.com>
2021-12-14 15:05:31 +0200

    responsiveness

--- src/components/JobDetails.js
+++ src/components/JobDetails.js
@@ -9,31 +9,31 @@ export const JobDetails = () => {
       <h3 style={{ color: "rgb(24, 24, 163)" }}>Junior developer</h3>
       <br></br>
       <div className="JOB_D">
-        <p>Kuranga ltd</p>
+        <p className="color"  >Kuranga ltd</p>
         <p>Full-time</p>
         <p>Junior developer</p>
         <button className="app-button" onClick={() => history.push("/job/detail")}>Apply</button>
       </div>
       <div className="JOB_D">
-        <p>BK ltd</p>
+        <p className="color">BK ltd</p>
         <p>Full-time</p>
         <p>Junior developer</p>
         <button className="app-button" onClick={() => history.push("/job/detail")}>Apply</button>
       </div>
       <div className="JOB_D">
-        <p>Urumuri</p>
+        <p className="color">Urumuri</p>
         <p>Full-time</p>
         <p>Junior developer</p>
         <button className="app-button" onClick={() => history.push("/job/detail")}>Apply</button>
       </div>
       <div className="JOB_D">
-        <p>Mashu</p>
+        <p className="color">Mashu</p>
         <p>Full-time</p>
         <p>Junior developer</p>
         <button className="app-button" onClick={() => history.push("/job/detail")}>Apply</button>
       </div>
       <div className="JOB_D">
-        <p>Digital felice</p>
+        <p className="color">Digital felice</p>
         <p>Full-time</p>
         <p>Junior developer</p>
         <button className="app-button" onClick={() => history.push("/job/detail")}>Apply</button>
