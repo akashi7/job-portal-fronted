33887f86f24ccbaf35912cfc71244c63373a2728
akashi7 <christiannseko@gmail.com>
2022-01-27 12:55:03 +0200

    fixed expired jobs

--- src/components/JobDetails.js
+++ src/components/JobDetails.js
@@ -1,4 +1,5 @@
 import { useHistory } from "react-router-dom";
+import moment from "moment";
 
 export const JobDetails = ({ Jobs }) => {
 
@@ -11,6 +12,11 @@ export const JobDetails = ({ Jobs }) => {
 
   const length = Jobs.categoryJobs.length;
 
+  let date = new Date();
+  let today = moment(date).format("YYYY/MM/DD");
+
+  let todayDate = Date.parse(today);
+
   return (
     <div className="allJ">
       {(length === 0) ? <div className="no-jobs">
@@ -18,16 +24,20 @@ export const JobDetails = ({ Jobs }) => {
       </div>
         : Jobs.categoryJobs.map(({ id, job_title, company_name, posted_on, due, expiry_date }) => {
           return (
-            <div key={id} className="job-det" >
-              <h3 style={{ color: "rgb(24, 24, 163)" }}>{job_title} </h3>
-              <div className="JOB_D">
-                <p className="color"  >{company_name} </p>
-                <p>on : {posted_on} </p>
-                <p>Due : {expiry_date}</p>
-                {due === 'old' ? <p style={{ color: "red" }} > Expired </p> :
-                  <button className="app-button" onClick={(e) => goSeeJob(id)}>Apply</button>}
-              </div>
-            </div>
+            <>
+              {Date.parse(expiry_date) <= todayDate ? (
+                <div key={id} className="job-det" >
+                  <h3 style={{ color: "rgb(24, 24, 163)" }}>{job_title} </h3>
+                  <div className="JOB_D">
+                    <p className="color"  >{company_name} </p>
+                    <p>on : {posted_on} </p>
+                    <p>Due : {expiry_date}</p>
+                    {due === 'old' ? <p style={{ color: "red" }} > Expired </p> :
+                      <button className="app-button" onClick={(e) => goSeeJob(id)}>Apply</button>}
+                  </div>
+                </div>
+              ) : ""}
+            </>
           );
         })}
     </div>
